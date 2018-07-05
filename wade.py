fe860364634059813fa681cb5ee0a5841f0be90d
GU Yu <ygu@thoughtworks.com>
2018-07-05 17:35:24 +0800

    add multi-thread capability.

--- wade.py
+++ wade.py
@@ -4,22 +4,43 @@ import time
 import datetime
 import sys
 import requests
+from multiprocessing import Pool
 
+def request_thread(i, time_long, url):
+    result = {'Thread':i, '2XX':0 ,'3XX':0, '4XX':0, '5XX':0}
+    print(result)
+    start_time = time.time()
+    while time.time() < start_time + float(time_long) * 60 + 1:
+        response = requests.get(url)
+        status_code = response.status_code
+        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        print("[{0}] Thread-{1}:{2}".format(st,i,status_code))
+        code = "{0}XX".format(str(status_code)[0])
+        result[code] = result[code] + 1
+        print(result)
+    
+    # return result
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action='version', version='%(prog)s 0.0.1')
     parser.add_argument("-t", "--time", help="count downtime duriation in minutes")
-    parser.add_argument("-u", "--url", help="full path of .csv file")
+    parser.add_argument("-u", "--url", help="url to test")
+    parser.add_argument("-n", "--thread_num", help="number of request thread")
     args = parser.parse_args()
+    pool = Pool(processes=int(args.thread_num))
+    results = []
+    for i in range(int(args.thread_num)):
+        result = pool.apply_async(request_thread, args=(i, args.time, args.url))
+        results.append(result)    
+    pool.close()
+    pool.join()
 
-    start_time = time.time()
+    # request_thread(1, args.time, args.url)
 
-    while time.time() < start_time + float(args.time) * 60 + 1:
-        response = requests.get(args.url)
-        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        print('[{0}]status:{1}'.format(st,response.status_code))
+    # for result in results:
+    #     print(result)
 
 if __name__ == '__main__':
     main()
