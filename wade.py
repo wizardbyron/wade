01c10915e6567fbef5d46b95125c753cc7e27b09
GU Yu <ygu@thoughtworks.com>
2018-07-10 13:49:43 +0800

    Print total result.

--- wade.py
+++ wade.py
@@ -6,41 +6,38 @@ import sys
 import requests
 from multiprocessing import Pool
 
-def request_thread(i, time_long, url):
-    result = {'Thread':i, '2XX':0 ,'3XX':0, '4XX':0, '5XX':0}
-    print(result)
+def request_process(i, time_long, url):
+    result = {'Process':i, '2XX':0 ,'3XX':0, '4XX':0, '5XX':0}
+
     start_time = time.time()
     while time.time() < start_time + float(time_long) * 60 + 1:
         response = requests.get(url)
         status_code = response.status_code
-        st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        print("[{0}] Thread-{1}:{2}".format(st,i,status_code))
         code = "{0}XX".format(str(status_code)[0])
         result[code] = result[code] + 1
-        print(result)
-    
-    # return result
+    end_time = time.time()
 
+    result['start time']=datetime.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+    result['end time']=datetime.datetime.fromtimestamp(end_time).strftime('%Y-%m-%d %H:%M:%S')
+    result['duriation']=datetime.datetime.fromtimestamp(end_time-start_time).strftime('%M:%S')
+    return result
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action='version', version='%(prog)s 0.0.1')
     parser.add_argument("-t", "--time", help="count downtime duriation in minutes")
     parser.add_argument("-u", "--url", help="url to test")
-    parser.add_argument("-n", "--thread_num", help="number of request thread")
+    parser.add_argument("-p", "--process_num", help="number of request process")
     args = parser.parse_args()
-    pool = Pool(processes=int(args.thread_num))
+    pool = Pool(processes=int(args.process_num))
     results = []
-    for i in range(int(args.thread_num)):
-        result = pool.apply_async(request_thread, args=(i, args.time, args.url))
-        results.append(result)    
+    for i in range(int(args.process_num)):
+        result = pool.apply_async(request_process, args=(i, args.time, args.url))
+        results.append(result)
     pool.close()
     pool.join()
-
-    # request_thread(1, args.time, args.url)
-
-    # for result in results:
-    #     print(result)
+    for i in range(int(args.process_num)):
+        print(results[i].get())
 
 if __name__ == '__main__':
     main()
